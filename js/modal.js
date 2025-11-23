43fe5daaea27deade6893b4c7447c75cacf8a2c6
XepiDoo <xepa3@proton.me>
2025-11-23 19:03:58 +0000

    New version - started with Adaptability

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
+const menuBtnClose = document.querySelector('menu-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
